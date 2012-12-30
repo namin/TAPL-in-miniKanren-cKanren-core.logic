4ab9b0d784a027c7ec1fabe8ebf2300ec8c8c075
Nada Amin <namin@alum.mit.edu>
2012-12-30 10:17:27 +0100

    bump to master version (beta5)

--- clojure-tapl/tapl/project.clj
+++ clojure-tapl/tapl/project.clj
@@ -2,6 +2,6 @@
   :description "TAPL in core.logic"
   :url "https://github.com/namin/TAPL-in-miniKanren-cKanren-core.logic"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.logic "0.8.0-beta4-SNAPSHOT"]
+                 [org.clojure/core.logic "0.8.0-beta5-SNAPSHOT"]
                  [org.clojure/core.match "0.2.0-alpha11"]]
   :dev-dependencies [[lein-swank "1.4.3"]])
