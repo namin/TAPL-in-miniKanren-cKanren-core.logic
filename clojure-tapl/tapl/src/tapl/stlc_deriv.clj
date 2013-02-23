0b3b2a30b5a951964d21ca5a33814b3309df49de
Nada Amin <namin@alum.mit.edu>
2013-02-23 15:54:46 +0100

    first steps towards a proof debugger

--- clojure-tapl/tapl/src/tapl/stlc_deriv.clj
+++ clojure-tapl/tapl/src/tapl/stlc_deriv.clj
@@ -29,3 +29,57 @@
                    ['typingo case2 gamma t2 ty11]]))])))
 
 (def typingo-deriv (solve-proof-for typingo-clause))
+
+(defn typingo-debug-clause [head tail]
+  (conde
+    [(fresh [x y]
+       (== head ['== x y])
+       (== x y)
+       (== tail ()))]
+    [(fresh [gamma t ty]
+       (== head ['lookupo gamma t ty])
+       (lookupo gamma t ty)
+       (== tail ()))]
+    [(fresh [case gamma t ty]
+       (== head ['typingo case gamma t ty])
+       (conde
+         [(== case 'T-Var)
+          (nomo t)
+          (== tail [['lookupo gamma t ty]])]
+         [(== case 'T-Abs)
+          (fresh [casex gammax ty1 ty2 t2]
+            (nom/fresh [x]
+              (== t `(~'fn ~(nom/tie x t2)))
+              (conso [x ty1] gamma gammax)
+              (== tail [['typingo casex gammax t2 ty2]
+                        ['== ty [:=> ty1 ty2]]])))]
+         [(== case 'T-App)
+           (fresh [case1 case2 ty1 ty2 ty11 ty12 t1 t2]
+             (== t `(~t1 ~t2))
+             (!= t1 'fn)
+             (== tail [['typingo case1 gamma t1 ty1]
+                       ['typingo case2 gamma t2 ty2]
+                       ['== ty1 [:=> ty11 ty12]]
+                       ['== ty2 ty11]
+                       ['== ty ty12]]))]))]))
+
+;; Adapted from logically.art.interpreters.meta/solve-proof-for.
+(defn debug-proof-for [clause]
+  (letfn [(solve0 [goal tree]
+            (solve [goal] tree))
+          (solve [goals tree]
+            (conde
+              [(== goals ())
+               (== tree ())]
+              [(fresh [g gs ts b tb]
+                 (conso g gs goals)
+                 (conda
+                   [(clause g b)
+                    (conso [g '<-- tb] ts tree)
+                    (solve b tb)]
+                   [(conso [g 'error] ts tree)])
+                 (solve gs ts))]))]
+    solve0))
+
+(def typingo-only-debug (debug-proof-for typingo-clause))
+(def typingo-debug (debug-proof-for typingo-debug-clause))
--- clojure-tapl/tapl/test/tapl/test/stlc_deriv.clj
+++ clojure-tapl/tapl/test/tapl/test/stlc_deriv.clj
@@ -29,3 +29,54 @@
                (typingo-deriv ['typingo c () `(~'fn ~(nom/tie x [x x])) t] d))))
         ())))
 
+(deftest test-typingo-only-debug
+  (is (= (read-string
+           (prn-str
+             (run* [q]
+               (fresh [c t d]
+                 (nom/fresh [x]
+                   (typingo-only-debug ['typingo c () `(~'fn ~(nom/tie x x)) t] d)
+                   (== q [c t d]))))))
+        '([T-Abs [:=> _0 _0] ([[typingo T-Abs () (fn  [a_1] a_1) [:=> _0 _0]] <-- ([[typingo T-Var ([a_2 _0]) a_2 _0] <-- ()])])])))
+  (is (= (read-string
+           (prn-str
+             (run* [q]
+               (fresh [c t d]
+                 (nom/fresh [x]
+                   (typingo-only-debug ['typingo c () `((~'fn ~(nom/tie x x)) (~'fn ~(nom/tie x x))) t] d)
+                   (== q [c t d]))))))
+        '([T-App [:=> _0 _0] ([[typingo T-App () ((fn [a_1] a_1) (fn [a_1] a_1)) [:=> _0 _0]] <-- ([[typingo T-Abs () (fn [a_1] a_1) [:=> [:=> _0 _0] [:=> _0 _0]]] <-- ([[typingo T-Var ([a_2 [:=> _0 _0]]) a_2 [:=> _0 _0]] <-- ()])] [[typingo T-Abs () (fn [a_1] a_1) [:=> _0 _0]] <-- ([[typingo T-Var ([a_3 _0]) a_3 _0] <-- ()])])])]))))
+
+(deftest test-typingo-debug
+  (is (= (read-string
+           (prn-str
+             (run* [q]
+               (fresh [c t d]
+                 (nom/fresh [x]
+                   (typingo-debug ['typingo c () `(~'fn ~(nom/tie x x)) t] d)
+                   (== q [c t d]))))))
+        '([T-Abs [:=> _0 _0] ([[typingo T-Abs () (fn [a_1] a_1) [:=> _0 _0]] <-- ([[typingo T-Var ([a_2 _0]) a_2 _0] <-- ([[lookupo ([a_2 _0]) a_2 _0] <-- ()])] [[== [:=> _0 _0] [:=> _0 _0]] <-- ()])])])))
+  (is (= (read-string
+           (prn-str
+             (run* [q]
+               (fresh [c t d]
+                 (nom/fresh [x]
+                   (typingo-debug ['typingo c () `((~'fn ~(nom/tie x x)) (~'fn ~(nom/tie x x))) t] d)
+                   (== q [c t d]))))))
+        '([T-App [:=> _0 _0] ([[typingo T-App () ((fn [a_1] a_1) (fn [a_1] a_1)) [:=> _0 _0]] <-- ([[typingo T-Abs () (fn [a_1] a_1) [:=> [:=> _0 _0] [:=> _0 _0]]] <-- ([[typingo T-Var ([a_2 [:=> _0 _0]]) a_2 [:=> _0 _0]] <-- ([[lookupo ([a_2 [:=> _0 _0]]) a_2 [:=> _0 _0]] <-- ()])] [[== [:=> [:=> _0 _0] [:=> _0 _0]] [:=> [:=> _0 _0] [:=> _0 _0]]] <-- ()])] [[typingo T-Abs () (fn [a_1] a_1) [:=> _0 _0]] <-- ([[typingo T-Var ([a_3 _0]) a_3 _0] <-- ([[lookupo ([a_3 _0]) a_3 _0] <-- ()])] [[== [:=> _0 _0] [:=> _0 _0]] <-- ()])] [[== [:=> [:=> _0 _0] [:=> _0 _0]] [:=> [:=> _0 _0] [:=> _0 _0]]] <-- ()] [[== [:=> _0 _0] [:=> _0 _0]] <-- ()] [[== [:=> _0 _0] [:=> _0 _0]] <-- ()])])])))
+  (is (= (read-string
+           (prn-str
+             (run* [q]
+               (fresh [c t d]
+                 (nom/fresh [x]
+                   (typingo-debug ['typingo c () `(~'fn ~(nom/tie x [x x])) t] d)
+                   (== q [c t d]))))))
+        '([T-Abs [:=> [:=> _0 _1] _1] ([[typingo T-Abs () (fn [a_2] [a_2 a_2]) [:=> [:=> _0 _1] _1]] <-- ([[typingo T-App ([a_3 [:=> _0 _1]]) (a_3 a_3) _1] <-- ([[typingo T-Var ([a_3 [:=> _0 _1]]) a_3 [:=> _0 _1]] <-- ([[lookupo ([a_3 [:=> _0 _1]]) a_3 [:=> _0 _1]] <-- ()])] [[typingo T-Var ([a_3 [:=> _0 _1]]) a_3 [:=> _0 _1]] <-- ([[lookupo ([a_3 [:=> _0 _1]]) a_3 [:=> _0 _1]] <-- ()])] [[== [:=> _0 _1] [:=> _0 _1]] <-- ()] [[== [:=> _0 _1] _0] error] [[== _1 _1] <-- ()])] [[== [:=> [:=> _0 _1] _1] [:=> [:=> _0 _1] _1]] <-- ()])])])))
+  (is (= (read-string
+           (prn-str
+             (run* [q]
+               (fresh [c t d]
+                 (nom/fresh [x y]
+                   (typingo-debug ['typingo c () `(~'fn ~(nom/tie x y)) t] d)
+                   (== q [c t d]))))))
+        '([T-Abs [:=> _0 _1] ([[typingo T-Abs () (fn [a_2] a_3) [:=> _0 _1]] <-- ([[typingo T-Var ([a_4 _0]) a_3 _1] <-- ([[lookupo ([a_4 _0]) a_3 _1] error])] [[== [:=> _0 _1] [:=> _0 _1]] <-- ()])])]))))
