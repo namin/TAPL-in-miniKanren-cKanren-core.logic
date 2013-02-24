19e19d21b3d909e6707debae1a50906eb700813b
Nada Amin <namin@alum.mit.edu>
2013-02-24 18:33:47 +0100

    use snapshot core.logic and bump logically

--- clojure-tapl/tapl/project.clj
+++ clojure-tapl/tapl/project.clj
@@ -2,7 +2,7 @@
   :description "TAPL in core.logic"
   :url "https://github.com/namin/TAPL-in-miniKanren-cKanren-core.logic"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.logic "0.8.0-rc2"]
-                 [logically "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.logic "0.8.0-rc3-SNAPSHOT"]
+                 [logically "0.1.1-SNAPSHOT"]
                  [org.clojure/core.match "0.2.0-alpha11"]]
   :dev-dependencies [[lein-swank "1.4.3"]])
