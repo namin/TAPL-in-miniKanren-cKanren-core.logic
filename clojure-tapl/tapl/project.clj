ff6f130954674db0f6d3c57dcd941f5edff0a2c9
Nada Amin <namin@alum.mit.edu>
2013-03-12 09:07:30 +0100

    use latest clojure version

--- clojure-tapl/tapl/project.clj
+++ clojure-tapl/tapl/project.clj
@@ -1,7 +1,7 @@
 (defproject tapl "1.0.0-SNAPSHOT"
   :description "TAPL in core.logic"
   :url "https://github.com/namin/TAPL-in-miniKanren-cKanren-core.logic"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.8.0-rc3-SNAPSHOT"]
                  [logically "0.1.1-SNAPSHOT"]
                  [org.clojure/core.match "0.2.0-alpha12"]]
