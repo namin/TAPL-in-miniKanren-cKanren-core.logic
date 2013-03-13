485f93724ad0e2a6625fca143f18f4c8598f339e
Nada Amin <namin@alum.mit.edu>
2013-03-13 20:29:35 +0100

    iteration in combinatory logic

--- clojure-tapl/tapl/src/tapl/quines_sk.clj
+++ clojure-tapl/tapl/src/tapl/quines_sk.clj
@@ -1,10 +1,15 @@
 (ns tapl.quines_sk
+  (:use [tapl.utils] :reload)
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic :exclude [is] :as l]
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom]))
 
 (def unl-hello
   "```````````.H.e.l.l.o. .w.o.r.l.di"
+)
+
+(def unl-hello-n
+  "```si`k``s.H``s.e``s.l``s.l``s.o``s. ``s.w``s.o``s.r``s.l``s.d``s.!``sii``si``si``si``si``si``si``si``si`ki"
 )
 
 (def unl-quine
@@ -14,28 +19,58 @@
 (defn unl2sk [x] (map (fn [c] (if (= c \`) '_ (symbol (str c)))) x))
 (defn sk2unl [x] (apply str (map (fn [s] (if (= s '_) "`" (str s))) x)))
 
-(def sk-quine (unl2sk unl-quine))
 (def sk-hello (unl2sk unl-hello))
+(def sk-hello-n (unl2sk unl-hello-n))
+(def sk-quine (unl2sk unl-quine))
 
-(defn appo [a1 a2 b1 b2 c1 c2]
-  (all
-    (== a2 b1)
-    (== a1 c1)
-    (== b2 c2)))
+(defn substo [e new a out]
+  (conde
+    [(nomo e) (== e a) (== new out)]
+    [(nomo e) (!= e a) (== e out)]
+    [(symbolo e) (== e out)]
+    [(== e ()) (== e out)]
+    [(fresh [h t ho to]
+       (conso h t e)
+       (conso ho to out)
+       (substo h new a ho)
+       (substo t new a to))]
+    [(fresh [b bo]
+       (nom/fresh [c]
+         (== e (nom/tie c b))
+         (== out (nom/tie c bo))
+         (nom/hash c a)
+         (nom/hash c new)
+         (substo b new a bo)))]))
+
+(defn doneo [in]
+  (fresh [a d]
+    (conso a d in)
+    (!= a '.)
+    (!= a '_)))
 
 (defn evalo [in1 in2 out1 out2]
   (fresh [a d]
     (conso a d in1)
     (conde
       [(== a '.)
-        (fresh [c r rout1 rout2]
+        (fresh [c r]
           (conso c r d)
-          (appo (lcons c rout1) rout1 rout1 rout2 out1 out2)
-          (evalo (lcons 'i r) in2 rout1 rout2))]
+          (symbolo c)
+          (evalo (lcons `(~'. ~c) r) in2 out1 out2))]
       [(== a '_)
-        (fresh [f rf arg rarg outf]
+        (fresh [f rf arg rarg fun fbody sarg outf outf2 outarg rest]
           (evalo d (lcons f rf) out1 outf)
-          (evalo rf (lcons arg rarg) outf out2)
-          (conde
-            [(== f 'i) (== (lcons arg rarg) in2)]))]
-      [(== a 'i) (== in1 in2) (== out1 out2)])))
+          (nom/fresh [x y z]
+            (conde
+              [(fresh [c] (== f `(~'. ~c)) (symbolo c) (== fun (nom/tie x `(~x))) (conso c outf2 outf))]
+              [(== f 'i) (== fun (nom/tie x `(~x))) (== outf2 outf)]
+              [(== f 'k) (== fun (nom/tie x `(~(nom/tie y `(~x))))) (== outf2 outf)]
+              [(== f 's) (== fun (nom/tie x `(~(nom/tie y `(~(nom/tie z `(~'_ ~'_ ~x ~z ~'_ ~y ~z))))))) (== outf2 outf)]
+              [(== f (nom/tie x fbody)) (== fun f) (== outf2 outf)])
+            (== fun (nom/tie x fbody))
+            (nom/hash x arg)
+            (substo fbody arg x sarg))
+          (evalo rf (lcons arg rarg) outf2 outarg)
+          (appendo sarg rarg rest)
+          (evalo rest in2 outarg out2))]
+      [(doneo in1) (== in1 in2) (== out1 out2)])))
--- clojure-tapl/tapl/test/tapl/test/quines_sk.clj
+++ clojure-tapl/tapl/test/tapl/test/quines_sk.clj
@@ -10,15 +10,31 @@
   (is (= unl-quine (sk2unl (unl2sk unl-quine)))))
 
 (deftest test-basics
-  (is (= (run* [q] (fresh [in2 out1] (evalo '(. h) in2 out1 ()) (== q [in2 out1])))
+  (is (= (run* [q] (fresh [in2 out1 out2] (evalo '(_ . h i) in2 out1 ()) (== q [in2 out1])))
         '([(i) (h)])))
-  (is (= (run* [q] (fresh [in2 out1 out2] (evalo '(_ i . h) in2 out1 ()) (== q [in2 out1])))
-        '([(i) (h)])))
-  (is (= (run* [q] (fresh [in2 out1] (evalo '(_ . h . e) in2 out1 ()) (== q [in2 out1])))
+  (is (= (run* [q] (fresh [in2 out1] (evalo '(_ _ . h . e i) in2 out1 ()) (== q [in2 out1])))
         '([(i) (h e)]))))
 
 (deftest test-hello
   (is (= (map #(apply str %)
            (run* [q] (evalo sk-hello '(i) q ())))
         '("Hello world"))))
+
+(deftest test-sk
+  (is (= (run* [q] (fresh [in2 out1] (evalo '(_ _ _ s k s i) in2 out1 ()) (== q [in2 out1])))
+        '([(i) ()])))
+  (is (= (run* [q] (fresh [in2 out1] (evalo '(_ _ k i s) in2 out1 ()) (== q [in2 out1])))
+        '([(i) ()])))
+  )
+
+(deftest test-iter
+  (is (= (run* [q] (fresh [in2 out1] (evalo (unl2sk "```si`k``s.*``sii``si``si``si``si``si``si``si``si`ki") in2 out1 ()) (== q [in2 out1])))
+        '([(i) (* * * * * * * *)])))
+  (is (= (run* [q] (fresh [in2 out1] (evalo (unl2sk "```si`k``s.*``sii``si``si`ki") in2 out1 ()) (== q [in2 out1])))
+        '([(i) (* *)]))))
+
+(deftest test-hello-n
+  (is (= (map #(apply str %)
+           (run* [q] (fresh [i o1 o2] (evalo sk-hello-n '(i) q ()))))
+        '("Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!"))))
 
