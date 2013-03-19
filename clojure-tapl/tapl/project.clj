0d70ce93c4194d80fb8375dda8ea40ec84e097c9
Nada Amin <namin@alum.mit.edu>
2013-03-19 03:21:46 +0100

    bump core.logic release

--- clojure-tapl/tapl/project.clj
+++ clojure-tapl/tapl/project.clj
@@ -2,8 +2,8 @@
   :description "TAPL in core.logic"
   :url "https://github.com/namin/TAPL-in-miniKanren-cKanren-core.logic"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.0-rc3"]
-                 [logically "0.1.2-SNAPSHOT"]
+                 [org.clojure/core.logic "0.8.1"]
+                 [logically "0.1.3-SNAPSHOT"]
                  [org.clojure/core.match "0.2.0-alpha12"]]
   :jvm-opts ["-Xss5M"]
 )
