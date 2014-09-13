fcdc465223e30dc00b9715c1ce0f624f98f28ca2
Nada Amin <namin@alum.mit.edu>
2014-09-13 19:05:58 +0200

    update deps

--- clojure-tapl/tapl/project.clj
+++ clojure-tapl/tapl/project.clj
@@ -1,9 +1,9 @@
 (defproject tapl "1.0.0-SNAPSHOT"
   :description "TAPL in core.logic"
   :url "https://github.com/namin/TAPL-in-miniKanren-cKanren-core.logic"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.8"]
                  [logically "0.1.3-SNAPSHOT"]
-                 [org.clojure/core.match "0.2.0-rc4"]]
+                 [org.clojure/core.match "0.2.1"]]
   :jvm-opts ["-Xss5M"]
 )
